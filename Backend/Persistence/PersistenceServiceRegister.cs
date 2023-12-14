b30ad9fddf25ee4f2daa18f952230136c1823a47
yohans hailu <emailyyohans@gmail.com>
2023-12-14 15:33:29 +0300

    [backend] [Refactor]: Moved connection string and token key to configuration file.

--- Backend/Persistence/PersistenceServiceRegister.cs
+++ Backend/Persistence/PersistenceServiceRegister.cs
@@ -7,10 +7,10 @@ namespace Persistence;
 public static class PersistenceServiceRegister
 {
     
-        public static IServiceCollection ConfigurePersistenceServices(this IServiceCollection services)
+        public static IServiceCollection ConfigurePersistenceServices(this IServiceCollection services, IConfiguration configuration)
         {
                 // adding the database context to dependency injection
-                services.AddDbContext<AppDbContext>();
+                services.AddDbContext<AppDbContext>(opt => opt.UseNpgsql(configuration.GetConnectionString("RenderPostgresConnectionString")));
                 return services;
         }
 }
