07876dcd0410c10a01e4f3586f2d993993f91627
yohans hailu <emailyyohans@gmail.com>
2023-12-14 12:04:30 +0300

    [backend] [Refactored]: Migrated from SQLite to PostgreSQL

--- Backend/Persistence/AppDbContext.cs
+++ Backend/Persistence/AppDbContext.cs
@@ -8,15 +8,15 @@ public class AppDbContext : IdentityDbContext<User>
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
-        optionsBuilder.UseSqlite("Data Source=mainDb.db");
-    }
-
-    protected override void OnModelCreating(ModelBuilder builder)
-    {
-        base.OnModelCreating(builder);
-
-        builder.Entity<User>() //Use your application user class here
-               .ToTable("AspNetUsers"); //Set the table name here
+        //TODO: move the connection string to config file 
+        //PGPASSWORD=sSopxoPBU8vuJZy6vzHwB20n1fsUEq5N psql -h dpg-clt9j921hbls73ebs8l0-a.frankfurt-postgres.render.com -U yohansh blogdb_ca1y
+        optionsBuilder.UseNpgsql("Host=dpg-clt9j921hbls73ebs8l0-a.frankfurt-postgres.render.com;" +
+                                 "Port=5432;" +
+                                 "Database=blogdb_ca1y;" +
+                                 "Username=yohansh ;" +
+                                 "Password=sSopxoPBU8vuJZy6vzHwB20n1fsUEq5N");
+        
+        
     }
 
     public DbSet<User> Users { get; set; }
